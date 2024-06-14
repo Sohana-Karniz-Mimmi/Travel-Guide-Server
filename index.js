3f2124a2ba7f583671d40da43dc7d6c226f4870d
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-06-15 00:50:44 +0600

    Get all wishlist data count from db for pagination

--- index.js
+++ index.js
@@ -204,6 +204,25 @@ async function run() {
 
       res.send(result);
     });
+    
+  // Get all wishlist data count from db
+  app.get("/wishlist/count/:email", async (req, res) => {
+    // const guideName = req.params.name;
+    const email = req?.params?.email;
+    const query = { email: email };
+    console.log(query);
+    const count = await wishlistCollection.countDocuments(query);
+
+    res.send({ count });
+  });
+
+  // delete a wishlist data
+  app.delete("/wishlist/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await wishlistCollection.deleteOne(query);
+    res.send(result);
+  });
 
     /*******************end***************************** */
 
